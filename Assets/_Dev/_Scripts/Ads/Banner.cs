db7295f91b48a1848643c13cea761024da347509
Batuhan <batuhank1997@hotmail.com>
2023-06-04 23:15:27 +0300

    Banner added

---
+++ Assets/_Dev/_Scripts/Ads/Banner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class Banner : MonoBehaviour
+{
+    // These ad units are configured to always serve test ads.
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+  private string _adUnitId = "unused";
+#endif
+
+    BannerView _bannerView;
+    
+    /// <summary>
+    /// Creates the banner view and loads a banner ad.
+    /// </summary>
+    public void LoadAd()
+    {
+        // create an instance of a banner view first.
+        if(_bannerView == null)
+        {
+            CreateBannerView();
+        }
+        
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("unity-admob-sample");
+
+        // send the request to load the ad.
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+    }
+
+    /// <summary>
+    /// Creates a 320x50 banner at top of the screen.
+    /// </summary>
+    private void CreateBannerView()
+    {
+        Debug.Log("Creating banner view");
+
+        // If we already have a banner, destroy the old one.
+        if (_bannerView != null)
+        {
+            DestroyAd();
+        }
+
+        // Create a 320x50 banner at top of the screen
+        _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.Bottom);
+    }
+    
+    /// <summary>
+    /// Destroys the ad.
+    /// </summary>
+    public void DestroyAd()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("Destroying banner ad.");
+            _bannerView.Destroy();
+            _bannerView = null;
+        }
+    }
+}
--- Assets/_Dev/_Scripts/Game/Managers/AdManager.cs
+++ Assets/_Dev/_Scripts/Game/Managers/AdManager.cs
@@ -1,9 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GoogleMobileAds.Api;
 using UnityEngine;
 
 public class AdManager : MonoBehaviour
 {
+    [SerializeField] private Banner banner;
+    
+    private void Awake()
+    {
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            Debug.LogWarning("MobileAds Init!");
+        });
+        
+        banner.LoadAd();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
